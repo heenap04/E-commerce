a7763164f56cdd80a082d484f75b21c09b0f7df3
Heena P <pawriyah2004@gmail.com>
2025-03-03 21:29:53 +0530

    backend & db connected

---
+++ backend/controllers/tokenController.js
@@ -0,0 +1,11 @@
+const {refreshToken} = require("../utils/token");
+
+// User Token Refresh Token
+exports.getRefreshToken = (req, res) => {
+    const refreshTokenInput = req.body.refreshToken;
+     refreshToken(refreshTokenInput).then((response)=>{
+         res.send(response);
+     }).catch(()=>{
+         res.status(500).send("Error Fetching Token");
+     });
+};
---
+++ backend/routes/userTokenRoutes.js
@@ -0,0 +1,10 @@
+// cartRoutes.js
+
+const express = require("express");
+const router = express.Router();
+const tokenController = require("../controllers/tokenController");
+
+// Route to get refreshToken
+router.post("/refreshToken", tokenController.getRefreshToken);
+
+module.exports = router;
